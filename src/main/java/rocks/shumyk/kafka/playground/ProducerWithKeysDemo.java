43c476d8d6c251fa7a52a38767217fea8a00a652
Shumyk <dima.shumyk@gmail.com>
2021-02-27 00:11:49 +0300

    added producer with keys demo

---
+++ src/main/java/rocks/shumyk/kafka/playground/ProducerWithKeysDemo.java
@@ -0,0 +1,70 @@
+package rocks.shumyk.kafka.playground;
+
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerWithKeysDemo {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProducerWithKeysDemo.class);
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+		// create producer props
+		final Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// create the producer
+		try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+			for (int i = 0; i < 10; i++) {
+				// send messages
+				final ProducerRecord<String, String> record = createMessage(i);
+				logger.info("Key: {}", record.key());
+				// id-0 -> partition 0
+				// id-1 -> partition 0
+				// id-2 -> partition 2
+				// id-3 -> partition 2
+				// id-4 -> partition 0
+				// id-5 -> partition 0
+				// id-6 -> partition 0
+				// id-7 -> partition 2
+				// id-8 -> partition 0
+				// id-9 -> partition 2
+
+				producer.send(record, messageCallback())
+					.get(); // block the .send() to make it synchronous - don't do this in production!
+				producer.flush();
+			}
+		}
+	}
+
+	private static ProducerRecord<String, String> createMessage(final int i) {
+		final String topic = "java_topic";
+		final String key = "id-" + i;
+		final String value = "this message is actually from java mate, " + i;
+		return new ProducerRecord<>(topic, key, value);
+	}
+
+	private static Callback messageCallback() {
+		return (recordMetadata, e) -> {
+			// executes every time a record is successfully sent or an exception is thrown
+			if (Objects.isNull(e))
+				// the record was successfully sent
+				logger.info("Received new metadata.\n Topic: {}.\n Partitions: {}.\n Offset: {}.\n Timestamp: {}.", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
+			else {
+				logger.error("Error while producing", e);
+			}
+		};
+	}
+}
